91565032bfcd55eb22c4a3b952c9648cefed606b
hiroyuki yamamoto <h.yama2396@gmail.com>
2021-08-23 07:13:07 +0900

    Add files via upload

--- release/ver 0.1.1/loadings/R/os_pca.R
+++ release/ver 0.1.1/loadings/R/os_pca.R
@@ -1,25 +1,24 @@
 # ------------
 #   OS-PCA
 # ------------
-os_pca <- function(X,Y,D,kappa=0.999, M=diag(1,nrow(X))){
-  
-  X <- scale(X)
+os_pca <- function(X,D,kappa=0.999, M=diag(1,nrow(X))){
+
   MX <- scale(M%*%X)
-  
+
   E <- (1-kappa)*diag(1,ncol(MX))+kappa*t(MX)%*%t(D)%*%D%*%MX
-  
+
   G <- chol(solve(E))
   W0 <- svd(G%*%t(MX)%*%MX)$v
-  
+
   # score
-  t <- X%*%W0
-  
+  t <- MX%*%W0
+
   R <- chol(E)
   z <- svd(t(MX)%*%MX%*%solve(R))$v
   W2 <- solve(R)%*%z
-  
+
   S <- MX%*%W2
-  
+
   list(P=W0, T=t, Q=W2, U=S)
-  
+
 }
