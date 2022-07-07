cc4fb9d9759a0fb838cc18472de8bbd1abc2ada0
hiroyuki yamamoto <h.yama2396@gmail.com>
2022-07-08 08:43:27 +0900

    Delete ver 0.2.1 directory

--- ver 0.2.1/loadings/R/multiplsrog_loading.R
+++
@@ -1,6 +0,0 @@
-source("C:/Users/yamamoto.HMT/Documents/R/multiomics/multipls_loading.R")
-
-multiplsrog_loading <- function(multiplsrog){
-  multiplsrog <- multipls_loading(multiplsrog)
-  return(multiplsrog)
-}
