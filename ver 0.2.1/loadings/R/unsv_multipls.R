1319f2e5ebf209028bdba51db430d6ba86d61aa1
hiroyuki yamamoto <h.yama2396@gmail.com>
2022-07-08 08:43:05 +0900

    Add files via upload

---
+++ ver 0.2.1/loadings/R/unsv_multipls.R
@@ -0,0 +1,21 @@
+library(geigen)
+
+source("C:/Users/yamamoto.HMT/Documents/R/multiomics/multipls_rog.R")
+
+### Unsupervised Multiset PLS
+unsv_multipls <- function(X,tau){
+  Y <- X[[length(X)]]
+  Y <- scale(Y,scale=TRUE)
+  X <- X[-length(X)]
+  p <- ncol(Y)
+  D <- diag(1, p)
+  
+  unsvmpls <- multipls_rog(X, Y, tau, D, kappa = 0)
+  unsvmpls$T <- c(unsvmpls$T,list(unsvmpls$U))
+  unsvmpls$P <- c(unsvmpls$P,list(unsvmpls$Q)) 
+  
+  unsvmpls$U <- NULL
+  unsvmpls$Q <- NULL
+
+  return(unsvmpls)
+}
