c643898708f0d1ef439de76960f7e98709604981
hiroyuki yamamoto <h.yama2396@gmail.com>
2023-01-31 19:13:18 +0900

    one-sided kernel PCA

---
+++ R/one_kpca.R
@@ -0,0 +1,24 @@
+one_kpca <- function(X,K){
+  
+  m <- nrow(X) # samplesize
+  
+  # one-sided KPCA (linear)
+  eig <- eigen(t(X)%*%K%*%X/m)
+  index <- order(eig$values, decreasing=TRUE)
+  ev1 <- eig$vectors[,index]
+  
+  T <- X%*%ev1
+  
+  # one-sided KPCA (kernel) 
+  eig <- geigen(t(K)%*%X%*%t(X)%*%K/m, K, symmetric = FALSE)
+  index <- order(eig$values, decreasing=TRUE)
+  ev <- eig$vectors[,index]
+  TK <- K%*%ev
+  
+  one_kpca <- NULL
+  one_kpca$T <- T
+  one_kpca$U <- TK
+  one_kpca$P <- ev1
+  
+  return(one_kpca)
+}
