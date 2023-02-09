f1707b44353423236bc76254faa96fbeee38530f
hiroyuki yamamoto <h.yama2396@gmail.com>
2023-02-09 20:05:25 +0900

    Add files via upload

--- R/pca_loading.R
+++ R/pca_loading.R
@@ -6,14 +6,10 @@ pca_loading <- function(pca){
   PC_loading <- lambda_sqrt*PC_weight
   # p-value
   n <- nrow(pca$x)
+  p_loading <- 2*stats::pt(abs(PC_loading)*sqrt(n-2)/sqrt(1-PC_loading^2),n-2,lower.tail=FALSE)
 
-  pv_PC_loading <- NULL
-  for(i in 1:ncol(PC_loading)){
-    p_loading <- 2*stats::pt(abs(PC_loading)*sqrt(n-2)/sqrt(1-PC_loading^2),n-2,lower.tail=FALSE)
-    pv_PC_loading <- cbind(pv_PC_loading,p_loading)
-  }
   pca$loading$R <- PC_loading
-  pca$loading$p.value <- pv_PC_loading
+  pca$loading$p.value <- p_loading
 
   return(pca)
 }
