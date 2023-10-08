673e9094eee14a5d600031c8a9131c1cf3437323
hiroyuki yamamoto <h.yama2396@gmail.com>
2023-10-08 15:22:50 +0900

    Add files via upload

--- R/onekpca_loadings.R
+++ R/onekpca_loadings.R
@@ -1,15 +1,15 @@
 onekpca_loading <- function(onekpca){
-  k <- min(ncol(onekpca$T),ncol(onekpca$U))
+  k <- min(ncol(onekpca$T), ncol(onekpca$U))
   n <- nrow(onekpca$T)
   ONEKPCA_loadings <- NULL
   p_ONEKPCA <- NULL
-  for(i in 1:k){
-    lambdax <- (1/2)*cov(onekpca$T[,i],onekpca$U[,i])
-    loading <- (2*lambdax*onekpca$P[,i]/sqrt(var(onekpca$U[,i])))
+  for (i in 1:k) {
+    lambdax <- (1/2) * stats::cov(onekpca$T[, i], onekpca$U[,i])
+    loading <- (2 * lambdax * onekpca$P[, i]/sqrt(stats::var(onekpca$U[,i])))
     p <- 2 * stats::pt(abs(loading) * sqrt(n - 2)/sqrt(1 - loading^2), n - 2, lower.tail = FALSE)
     ONEKPCA_loadings <- cbind(ONEKPCA_loadings, loading)
     p_ONEKPCA <- cbind(p_ONEKPCA, p)
-  }  
+  }
   onekpca$loading$R <- ONEKPCA_loadings
   onekpca$loading$p.value <- p_ONEKPCA
   return(onekpca)
--- R/unsv_multipls.R
+++ R/unsv_multipls.R
@@ -1,16 +1,13 @@
 unsv_multipls <- function(X,tau){
   Y <- X[[length(X)]]
-  Y <- scale(Y,scale=TRUE)
+  Y <- scale(Y, scale = TRUE)
   X <- X[-length(X)]
   p <- ncol(Y)
   D <- diag(1, p)
-
   unsvmpls <- multipls_rog(X, Y, tau, D, kappa = 0)
-  unsvmpls$T <- c(unsvmpls$T,list(unsvmpls$U))
-  unsvmpls$P <- c(unsvmpls$P,list(unsvmpls$Q))
-
+  unsvmpls$T <- c(unsvmpls$T, list(unsvmpls$U))
+  unsvmpls$P <- c(unsvmpls$P, list(unsvmpls$Q))
   unsvmpls$U <- NULL
   unsvmpls$Q <- NULL
-
   return(unsvmpls)
 }
