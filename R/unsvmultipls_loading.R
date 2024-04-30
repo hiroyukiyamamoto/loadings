76e7ea3697b011191345f14c4250359af243d1a7
hiroyuki yamamoto <h.yama2396@gmail.com>
2024-04-30 23:14:15 +0900

    Add files via upload

--- R/unsvmultipls_loading.R
+++ R/unsvmultipls_loading.R
@@ -1,7 +1,13 @@
-unsvmultipls_loading <- function(unsvmultipls){
-  unsvmultipls$U <- unsvmultipls$T[[length(unsvmultipls$T)]]
-  unsvmultipls$Q <- unsvmultipls$P[[length(unsvmultipls$P)]]
+unsvmultipls_loading <- function (unsvmpls) 
+{
+  unsvmultipls <- unsvmpls
+  unsvmultipls$U <- unsvmpls$T[[length(unsvmpls$T)]]
+  unsvmultipls$Q <- unsvmpls$P[[length(unsvmpls$P)]]
+  unsvmultipls$T <- unsvmpls$T[-length(unsvmpls$T)]
+  unsvmultipls$P <- unsvmpls$P[-length(unsvmpls$P)]
   unsvmultipls <- multipls_loading(unsvmultipls)
+  unsvmultipls$T <- unsvmpls$T
+  unsvmultipls$P <- unsvmpls$P
   unsvmultipls$U <- NULL
   unsvmultipls$Q <- NULL
   return(unsvmultipls)
